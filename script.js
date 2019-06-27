36a6019e3ff131d0e4bb2f76cd783678a7458c8a
MarcinNidecki <marcin.nidecki90@gmail.com>
2019-06-27 18:55:03 +0200

    Initial commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'http://pure-sea-14800.herokuapp.com/v1/task/';
+  var apiRoot = 'https://pure-sea-14800.herokuapp.com/v1/task/';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
