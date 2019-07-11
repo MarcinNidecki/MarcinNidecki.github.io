887302867c6ab396c53a6eb16bf172f453d98220
MarcinNidecki <marcin.nidecki90@gmail.com>
2019-07-11 14:48:11 +0200

    23.1

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
     $.ajax({
       url: requestUrl,
       method: 'GET',
-      contentType: 'application/json; charset=utf-8',
+      contentType: 'application/json',
       success: function(boards) { callback(callbackArgs, boards); }
     });
   }
@@ -68,7 +68,7 @@ $(document).ready(function() {
     $.ajax({
       url: requestUrl,
       method: 'GET',
-      contentType: "application/json; charset=utf-8",
+      contentType: "application/json",
       success: function(tasks) {
         tasks.forEach(task => {
           availableTasks[task.id] = task;
